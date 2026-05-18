4f83f0b39f25ae1cad8c8c396ff0b61a4df824d8
Pratigya Adhikari <pratigya144@gmail.com>
2026-05-18 11:51:03 +0545

    admin customization, python shell

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Recipe
 # Register your models here.
-admin.site.register(Recipe)
+
+# @admin.register(Recipe)        admin.site.register(Recipe, RecipeAdmin) narakhni vaya yo line (decoretor) rakh ne hunxa
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['id','name','ingredients','time']
+    list_filter = ['time']
+    search_fields = ['name','ingredients','time']
+    list_per_page = 10
+    
+    
+admin.site.register(Recipe, RecipeAdmin)
