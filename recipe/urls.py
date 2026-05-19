a83256fecc2333ad1b0046a480b737266dcfc2fb
Pratigya Adhikari <pratigya144@gmail.com>
2026-05-19 22:55:07 +0545

    update data

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import recipe_list,create
+from .views import recipe_list,create,update
 
 urlpatterns = [
     path('recipe/',recipe_list),
     path('recipe/create/',create),
+    path('recipe/<id>/',update),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -22,5 +22,24 @@ def create(request):
         Recipe.objects.create(name = name, ingredients = ingredient, instruction = instruction, time  = time, level = level)    # Recipe ma vako field_name = user bata aako name_variable mathi vako
         return redirect('/recipe/')
    #get process
-
     return render(request,"create.html")
+
+def update(request, id):
+    recipe = Recipe.objects.get(id = id)
+    context = {
+        "recipes":recipe
+    }
+    if request.method == "POST":
+        name = request.POST.get('name')     # form ma create gareko name lekhni
+        ingredient = request.POST.get('ingredient')
+        instruction = request.POST.get('instruction')
+        time =request.POST.get('time')
+        level = request.POST.get('level') 
+        recipe.name = name
+        recipe.ingredients = ingredient
+        recipe.instruction = instruction
+        recipe.time = time
+        recipe.level = level
+        recipe.save()
+        return redirect('/recipe/')
+    return render(request,"update.html",context)
